127fc6ec4b2bc113703ce277a180d69f7e19477f
Marry705 <marry705chilo@gmail.com>
2018-09-20 02:12:30 +0300

    function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,70 @@
 class Sorter {
-  constructor() {
-    // your implementation
+  constructor() { //конструктор
+    this._arr=[];
+    this._len=0;
   }
 
-  add(element) {
-    // your implementation
+  add(element) { // добавление элемент
+     this._arr.push(element);
+     this._len++;
   }
 
-  at(index) {
-    // your implementation
+  at(index) { //найти эл-т по индексу
+      if (index<this._len && index>=0) {return this._arr[index];}
+      else {return "Еhere is no element with such index";}
   }
 
-  get length() {
-    // your implementation
+  get length() { // не проходит тест
+    return this._len;
   }
 
-  toArray() {
-    // your implementation
+  toArray() { 
+ //   console.log(this._arr); // функция вывода на печать
+      return this._arr;
   }
 
-  sort(indices) {
-    // your implementation
+  sort(indices=[]) {
+    if (indices.length===1 || indices.length===0) {
+      return this._arr;
+    }
+    else {
+      var m = indices.length;
+    	for (var i = 0; i < m-1; i++){ 
+        for (var j = 0; j < m-1-i; j++){
+          if (indices[j+1] < indices[j]){
+            var t = indices[j+1]; 
+            indices[j+1] = indices[j]; 
+            indices[j] = t; 
+          }
+        }
+      }  
+       //indices.sort();
+      let boof = [];
+      var n = this._len;
+      for (var i = 0; i < n; i++){
+        for (var k = 0; k < m; k++){
+          if (i===indices[k]) {boof.push(this._arr[i]);}
+        }
+      }
+      m = boof.length;
+      for (var i = 0; i < m-1; i++){ 
+        for (var j = 0; j < m-1-i; j++){
+          if (boof[j+1] < boof[j]){
+            var t = boof[j+1]; 
+            boof[j+1] = boof[j]; 
+            boof[j] = t; 
+          }
+        }
+      }  
+      var j=0;
+      m = indices.length;
+		  for (var i = 0; i < n; i++){
+        for (var k = 0; k < m; k++){
+          if (i===indices[k]) {this._arr[i]=boof[j]; j++;}
+        }
+      }
+    }
+  return this._arr;
   }
 
   setComparator(compareFunction) {
